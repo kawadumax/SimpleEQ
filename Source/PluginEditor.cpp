266e72f259597eda66463c647e90b3ef14321433
おるとろ <kazmax7@gmail.com>
2023-03-10 09:31:55 +0900

    ボタンとテスト追加

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -16,6 +16,13 @@ MultibandReverbAudioProcessorEditor::MultibandReverbAudioProcessorEditor (Multib
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
     setSize (400, 300);
+
+    // Create a TextButton
+    myButton.setButtonText("Click me!");
+    myButton.addListener(this);
+
+    // Add the TextButton to this component
+    addAndMakeVisible(myButton);
 }
 
 MultibandReverbAudioProcessorEditor::~MultibandReverbAudioProcessorEditor()
@@ -27,14 +34,26 @@ void MultibandReverbAudioProcessorEditor::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
     g.setColour (juce::Colours::white);
     g.setFont (15.0f);
     g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+
+
 }
 
 void MultibandReverbAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+    // 
+    // Set the bounds of the TextButton
+    myButton.setBounds(10, 10, 100, 30);
+}
+
+void MultibandReverbAudioProcessorEditor::buttonClicked(juce::Button* button)
+{
+    if (button == &myButton) {
+        // Handle button click here
+        std::cout << "Button clicked!\n";
+    }
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -14,7 +14,8 @@
 //==============================================================================
 /**
 */
-class MultibandReverbAudioProcessorEditor  : public juce::AudioProcessorEditor
+class MultibandReverbAudioProcessorEditor  : public juce::AudioProcessorEditor,
+    public juce::Button::Listener
 {
 public:
     MultibandReverbAudioProcessorEditor (MultibandReverbAudioProcessor&);
@@ -24,10 +25,14 @@ public:
     void paint (juce::Graphics&) override;
     void resized() override;
 
+    void buttonClicked(juce::Button* button) override;
+
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     MultibandReverbAudioProcessor& audioProcessor;
+    juce::TextButton myButton;
+    juce::ComboBox myComboBox;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MultibandReverbAudioProcessorEditor)
 };
